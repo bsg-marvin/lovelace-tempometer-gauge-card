90fba2a7983e18dfee06f161d9fcf3cb2b2c1186
Filip Jurik <filip.jurik@bsg.ox.ac.uk>
2022-05-12 18:36:21 +0100

    Update tempometer-gauge-card.js

--- tempometer-gauge-card.js
+++ tempometer-gauge-card.js
@@ -41,7 +41,7 @@ class TempometerGaugeCard extends HTMLElement {
       if (entityMaxParts.attribute) cardConfig.maxAttribute = entityMaxParts.attribute;
     }
 	  
-    if (.icon2 !== 'mdi:battery-50') {
+    if (object.icon2 !== 'mdi:battery-50') {
       	var icon_color = 'yellow'
     } else {
       	var icon_color = "var(--paper-item-icon-color)";
